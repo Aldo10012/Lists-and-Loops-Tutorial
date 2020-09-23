4d4d70ca5cd29ae07f3fa921f6f7dea4ddeed7f5
Alberto <aldo10012@gmail.com>
2020-09-22 17:47:08 -0700

    first commit

---
+++ list_loops.py
@@ -0,0 +1,28 @@
+# Question 1
+songs = ["ROCKSTAR", "Do It", "For The Night"]
+
+# Question 2
+print( songs[0] )
+print( songs[2] )
+print( songs[1:3] )
+
+# Question 3
+songs[2] = "Battle Symphony"
+print(songs)
+
+# Question 4
+songs.append("Old Town Road")
+songs.append("Foot Loose")
+songs.append("Honey I'm Good")
+del songs[1]
+
+# Question 5
+
+
+# Question 6
+animals = ['lion', 'tiger', 'panther']
+animals.append('bob cat')
+print(animals[2])
+del animals[0]
+for animal in animals:
+    print(animal)
